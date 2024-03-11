6936b67a8f93b105300507cde8a12ddd850d1e21
Blake Loring <blake@parsed.uk>
2024-03-12 04:54:25 +0800

    Skeleton of the 'op' opcodes

--- lib/decode_and_execute.ml
+++ lib/decode_and_execute.ml
@@ -1,6 +1,7 @@
 open! Core
 open Hardcaml
-open! Signal
+open Signal
+open Always
 
 module Make
     (Hart_config : Hart_config_intf.S)
@@ -14,7 +15,9 @@ struct
       ; hart_to_memory_controller : 'a Memory.Tx_bus.Rx.t
            [@rtlprefix "hart_to_memory_controller"]
       ; enable : 'a
-      ; instruction : 'a (* TODO: This is assuming Rv32i, I guess in practice this should be the length of the longest instruction we support? *) [@bits 32]
+      ; instruction : 'a
+           (* TODO: This is assuming Rv32i, I guess in practice this should be the length of the longest instruction we support? *)
+           [@bits 32]
       ; registers : 'a Registers.t
       }
     [@@deriving sexp_of, hardcaml]
@@ -32,7 +35,27 @@ struct
     [@@deriving sexp_of, hardcaml]
   end
 
-  let create _scope (_i : _ I.t) = assert false
+  let op_instructions registers instruction = [] |> proc
+
+  let create _scope (_i : _ I.t) =
+    let finished = Variable.wire ~default:(zero 1) in
+    let new_registers = Registers.Of_always.wire zero in
+    compile
+      [ when_
+          enable
+          [ when_
+              (Decoder.opcode i.instruction =:. Opcodes.op)
+              [ Registers.Of_always.assign new_registers (op i.registers i.instructions)
+              ; finished <--. 1
+              ]
+          ]
+      ];
+    { O.memory_controller_to_hart = Memory.Rx_bus.Rx.Of_signal.of_int 1
+    ; hart_to_memory_controller = Memory.Tx_bus.Tx.of_int 1
+    ; finished = finished.value
+    ; new_registers = Registers.Of_always.value new_registers
+    }
+  ;;
 
   let hierarchical ~instance (scope : Scope.t) (input : Signal.t I.t) =
     let module H = Hierarchy.In_scope (I) (O) in
