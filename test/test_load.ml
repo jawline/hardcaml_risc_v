decba93102981b529dd7ff165de996b981febe27
Blake Loring <blake@parsed.uk>
2024-03-19 23:44:18 +0800

    Test load skeleton

--- test/test_load.ml
+++ test/test_load.ml
@@ -17,3 +17,18 @@ module Memory_controller = Memory_controller.Make (struct
   end)
 
 module Load = Load.Make (Hart_config) (Memory_controller)
+
+let create_sim () =
+  let module Sim = Cyclesim.With_interface (Load.I) (Load.O) in
+  Sim.create (Load.create (Scope.create ()))
+;;
+
+let print (_outputs : _ Load.O.t) = assert false
+
+let test ~source ~funct3 sim =
+  let inputs : _ Load.I.t = Cyclesim.inputs sim in
+  inputs.source := of_int ~width:32 source;
+  inputs.funct3 := of_int ~width:3 funct3;
+  (* TODO: drive and create the memory controller *)
+  assert false
+;;
