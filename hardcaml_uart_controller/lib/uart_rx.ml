b688a17a54b5e60dca70a94f9f861b718027be88
Blake Loring <blake@parsed.uk>
2024-04-03 00:30:16 +0800

    Implemenet rx, begin writing the joined test

--- hardcaml_uart_controller/lib/uart_rx.ml
+++ hardcaml_uart_controller/lib/uart_rx.ml
@@ -50,21 +50,82 @@ struct
     ==:. 0
   ;;
 
-  let create (scope : Scope.t) ({ I.clock; clear; uart_rx = _ } : _ I.t) =
+  let create (scope : Scope.t) ({ I.clock; clear; uart_rx } : _ I.t) =
     let ( -- ) = Scope.naming scope in
     let reg_spec = Reg_spec.create ~clock ~clear () in
+    let reg_spec_no_clear = Reg_spec.create ~clock () in
     let current_state = State_machine.create (module State) reg_spec in
     let switch_cycle = switch_cycle reg_spec in
+    let data = Variable.reg ~width:8 reg_spec_no_clear in
+    let which_data_bit = Variable.reg ~width:3 reg_spec_no_clear in
+    let which_stop_bit = Variable.reg ~width:2 reg_spec_no_clear in
+    (* Data with which_data_bit replaced with the current uart_rx *)
+    let data_with_new_data_bit =
+      mux_init
+        ~f:(fun index ->
+          let bits = split_lsb ~part_width:1 data.value in
+          concat_lsb (List.take bits index @ [ uart_rx ] @ List.drop bits (index + 1)))
+        which_data_bit.value
+        8
+    in
+    (* The parity bit should always = the RX parity bit if Config.include_parity_bit is set *)
+    let parity_bit = Variable.reg ~width:1 reg_spec_no_clear in
+    let rx_parity_bit = Variable.reg ~width:1 reg_spec_no_clear in
+    let parity_bit_matches =
+      if Config.include_parity_bit
+      then parity_bit.value ==: rx_parity_bit.value
+      else one 1
+    in
+    let stop_bit_not_stable = Variable.reg ~width:1 reg_spec_no_clear in
     ignore (current_state.current -- "current_state" : Signal.t);
+    let data_out_valid = Variable.wire ~default:(zero 1) in
+    let data_out = Variable.wire ~default:(zero 8) in
     compile
       [ current_state.switch
-          [ State.Waiting_for_start_bit, [ when_ switch_cycle [] ]
-          ; State.Waiting_for_data_bits, [ when_ switch_cycle [] ]
-          ; State.Waiting_for_parity_bit, [ when_ switch_cycle [] ]
-          ; State.Waiting_for_stop_bits, [ when_ switch_cycle [] ]
+          [ ( State.Waiting_for_start_bit
+            , [ data <--. 0
+              ; parity_bit <--. 0
+              ; rx_parity_bit <--. 0
+              ; which_data_bit <--. 0
+              ; stop_bit_not_stable <--. 0
+              ; which_stop_bit <--. 0
+              ; when_
+                  (switch_cycle &: (uart_rx ==:. 0))
+                  [ current_state.set_next State.Waiting_for_data_bits ]
+              ] )
+          ; ( State.Waiting_for_data_bits
+            , [ when_
+                  switch_cycle
+                  [ parity_bit <-- parity_bit.value +: uart_rx
+                  ; data <-- data_with_new_data_bit
+                  ; (if Config.include_parity_bit
+                     then current_state.set_next Waiting_for_parity_bit
+                     else current_state.set_next Waiting_for_stop_bits)
+                  ]
+              ] )
+          ; ( State.Waiting_for_parity_bit
+            , [ when_
+                  switch_cycle
+                  [ rx_parity_bit <-- uart_rx
+                  ; current_state.set_next Waiting_for_stop_bits
+                  ]
+              ] )
+          ; ( State.Waiting_for_stop_bits
+            , [ when_
+                  switch_cycle
+                  [ which_stop_bit <-- which_stop_bit.value +:. 1
+                  ; when_ (uart_rx ==:. 0) [ stop_bit_not_stable <--. 1 ]
+                  ; when_
+                      (which_stop_bit.value ==:. Config.stop_bits)
+                      [ data_out_valid
+                        <-- (~:(stop_bit_not_stable.value) &: parity_bit_matches)
+                      ; current_state.set_next Waiting_for_start_bit
+                      ]
+                  ]
+              ] )
           ]
       ];
-    { O.data_out_valid = assert false; data_out = assert false }
+    { O.data_out_valid = data_out_valid.value; data_out = data_out.value }
   ;;
 
   let hierarchical ~instance (scope : Scope.t) (input : Signal.t I.t) =
