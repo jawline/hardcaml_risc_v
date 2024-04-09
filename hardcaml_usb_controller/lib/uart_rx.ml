3a78c0a40155c9e9780e007a4c366d66fae37842
Blake Loring <blake@parsed.uk>
2024-04-09 23:23:00 +0800

    Start carving out the skeleton of a USB controller

---
+++ hardcaml_usb_controller/lib/uart_rx.ml
@@ -0,0 +1,150 @@
+(** This implements an 8-bit UART receiver at a fixed baud rate given a
+    fixed input frequency. The functor specifies whether to expect and validate
+    a parity bit and a stop bit. *)
+open! Core
+
+open Hardcaml
+open Signal
+open Always
+
+module Make (Config : sig
+    val clock_frequency : int
+    val baud_rate : int
+    val include_parity_bit : bool
+    val stop_bits : int
+  end) =
+struct
+  let switching_frequency = Config.clock_frequency / Config.baud_rate
+
+  module I = struct
+    type 'a t =
+      { clock : 'a
+      ; clear : 'a
+      ; uart_rx : 'a
+      }
+    [@@deriving sexp_of, hardcaml]
+  end
+
+  module O = struct
+    type 'a t =
+      { data_out_valid : 'a
+      ; data_out : 'a [@bits 8]
+      ; parity_error : 'a
+      ; stop_bit_unstable : 'a
+      }
+    [@@deriving sexp_of, hardcaml]
+  end
+
+  module State = struct
+    type t =
+      | Waiting_for_start_bit
+      | Waiting_for_data_bits
+      | Waiting_for_parity_bit
+      | Waiting_for_stop_bits
+    [@@deriving sexp, enumerate, compare]
+  end
+
+  let switch_cycle spec =
+    if switching_frequency = 1
+    then vdd
+    else (
+      let bits_to_repr_switching_frequency = Int.ceil_log2 switching_frequency in
+      (reg_fb ~width:bits_to_repr_switching_frequency ~f:(fun t ->
+         mod_counter ~max:(switching_frequency - 1) t))
+        spec
+      ==:. 0)
+  ;;
+
+  let create (scope : Scope.t) ({ I.clock; clear; uart_rx } : _ I.t) =
+    let ( -- ) = Scope.naming scope in
+    let reg_spec = Reg_spec.create ~clock ~clear () in
+    let reg_spec_no_clear = Reg_spec.create ~clock () in
+    let current_state = State_machine.create (module State) reg_spec in
+    let switch_cycle = switch_cycle reg_spec -- "switch_cycle" in
+    let data = Variable.reg ~width:8 reg_spec_no_clear in
+    let which_data_bit = Variable.reg ~width:3 reg_spec_no_clear in
+    let which_stop_bit = Variable.reg ~width:2 reg_spec_no_clear in
+    (* Data with which_data_bit replaced with the current uart_rx *)
+    let data_with_new_data_bit =
+      mux_init
+        ~f:(fun index ->
+          let bits = split_lsb ~part_width:1 data.value in
+          concat_lsb (List.take bits index @ [ uart_rx ] @ List.drop bits (index + 1)))
+        which_data_bit.value
+        8
+      -- "data_with_new_bit"
+    in
+    (* The parity bit should always = the RX parity bit if Config.include_parity_bit is set *)
+    let parity_bit = Variable.reg ~width:1 reg_spec_no_clear in
+    let rx_parity_bit = Variable.reg ~width:1 reg_spec_no_clear in
+    let parity_bit_matches =
+      if Config.include_parity_bit
+      then parity_bit.value ==: rx_parity_bit.value
+      else one 1
+    in
+    let stop_bit_not_stable = Variable.reg ~width:1 reg_spec_no_clear in
+    ignore (current_state.current -- "current_state" : Signal.t);
+    let data_out_valid = Variable.wire ~default:(zero 1) in
+    let parity_error = Variable.wire ~default:(zero 1) in
+    compile
+      [ current_state.switch
+          [ ( State.Waiting_for_start_bit
+            , [ data <--. 0
+              ; parity_bit <--. 0
+              ; rx_parity_bit <--. 0
+              ; which_data_bit <--. 0
+              ; stop_bit_not_stable <--. 0
+              ; which_stop_bit <--. 0
+              ; when_
+                  (switch_cycle &: (uart_rx ==:. 0))
+                  [ current_state.set_next State.Waiting_for_data_bits ]
+              ] )
+          ; ( State.Waiting_for_data_bits
+            , [ when_
+                  switch_cycle
+                  [ parity_bit <-- parity_bit.value +: uart_rx
+                  ; data <-- data_with_new_data_bit
+                  ; which_data_bit <-- which_data_bit.value +:. 1
+                  ; when_
+                      (which_data_bit.value -- "which_data_bit" ==:. 7)
+                      [ (if Config.include_parity_bit
+                         then current_state.set_next Waiting_for_parity_bit
+                         else current_state.set_next Waiting_for_stop_bits)
+                      ]
+                  ]
+              ] )
+          ; ( State.Waiting_for_parity_bit
+            , [ when_
+                  switch_cycle
+                  [ rx_parity_bit <-- uart_rx
+                  ; current_state.set_next Waiting_for_stop_bits
+                  ]
+              ] )
+          ; ( State.Waiting_for_stop_bits
+            , [ when_
+                  switch_cycle
+                  [ which_stop_bit <-- which_stop_bit.value +:. 1
+                  ; when_ (uart_rx ==:. 0) [ stop_bit_not_stable <--. 1 ]
+                  ; when_
+                      (which_stop_bit.value ==:. Config.stop_bits)
+                      [ data_out_valid
+                        <-- (~:(stop_bit_not_stable.value) &: parity_bit_matches)
+                      ; parity_error <-- ~:parity_bit_matches
+                      ; current_state.set_next Waiting_for_start_bit
+                      ]
+                  ]
+              ] )
+          ]
+      ];
+    { O.data_out_valid = data_out_valid.value
+    ; parity_error = parity_error.value
+    ; stop_bit_unstable = stop_bit_not_stable.value
+    ; data_out = data.value
+    }
+  ;;
+
+  let hierarchical ~instance (scope : Scope.t) (input : Signal.t I.t) =
+    let module H = Hierarchy.In_scope (I) (O) in
+    H.hierarchical ~scope ~name:"Uart_tx" ~instance create input
+  ;;
+end
---
+++ hardcaml_usb_controller/test/test_uart_rx.ml
@@ -0,0 +1,126 @@
+open! Core
+open Hardcaml
+open Hardcaml_waveterm
+open Hardcaml_uart_controller
+open! Bits
+
+let debug = false
+
+let test ~name ~clock_frequency ~baud_rate ~include_parity_bit ~stop_bits ~all_inputs =
+  let module Config = struct
+    (* This should trigger a switch every other cycle. *)
+    let clock_frequency = clock_frequency
+    let baud_rate = baud_rate
+    let include_parity_bit = include_parity_bit
+    let stop_bits = stop_bits
+  end
+  in
+  let module Uart_tx = Uart_tx.Make (Config) in
+  let module Uart_rx = Uart_rx.Make (Config) in
+  let module Machine = struct
+    module I = struct
+      type 'a t =
+        { clock : 'a
+        ; clear : 'a
+        ; data_in_valid : 'a
+        ; data_in : 'a [@bits 8]
+        }
+      [@@deriving sexp_of, hardcaml]
+    end
+
+    module O = struct
+      type 'a t =
+        { data_out_valid : 'a
+        ; data_out : 'a [@bits 8]
+        ; parity_error : 'a
+        ; stop_bit_unstable : 'a
+        }
+      [@@deriving sexp_of, hardcaml]
+    end
+
+    let create (scope : Scope.t) { I.clock; clear; data_in_valid; data_in } =
+      let { Uart_tx.O.uart_tx; _ } =
+        Uart_tx.hierarchical
+          ~instance:"tx"
+          scope
+          { Uart_tx.I.clock; clear; data_in_valid; data_in }
+      in
+      let { Uart_rx.O.data_out_valid; data_out; parity_error; stop_bit_unstable } =
+        Uart_rx.hierarchical
+          ~instance:"rx"
+          scope
+          { Uart_rx.I.clock; clear; uart_rx = uart_tx }
+      in
+      { O.data_out_valid; data_out; parity_error; stop_bit_unstable }
+    ;;
+  end
+  in
+  let create_sim () =
+    let module Sim = Cyclesim.With_interface (Machine.I) (Machine.O) in
+    Sim.create
+      ~config:Cyclesim.Config.trace_all
+      (Machine.create
+         (Scope.create ~auto_label_hierarchical_ports:true ~flatten_design:true ()))
+  in
+  let sim = create_sim () in
+  let waveform, sim = Waveform.create sim in
+  let inputs : _ Machine.I.t = Cyclesim.inputs sim in
+  let outputs : _ Machine.O.t = Cyclesim.outputs sim in
+  let all_outputs = ref [] in
+  List.iter
+    ~f:(fun input ->
+      inputs.data_in_valid := of_int ~width:1 1;
+      inputs.data_in := of_int ~width:8 input;
+      Cyclesim.cycle sim;
+      inputs.data_in_valid := of_int ~width:1 0;
+      let rec loop_until_finished acc n =
+        if n = 0
+        then List.rev acc
+        else (
+          Cyclesim.cycle sim;
+          let acc =
+            if Bits.to_bool !(outputs.data_out_valid)
+               || Bits.to_bool !(outputs.parity_error)
+            then Bits.to_int !(outputs.data_out) :: acc
+            else acc
+          in
+          loop_until_finished acc (n - 1))
+      in
+      let outputs = loop_until_finished [] 100 in
+      all_outputs := !all_outputs @ outputs)
+    all_inputs;
+  print_s [%message "" ~_:(!all_outputs : int list)];
+  if debug
+  then Waveform.expect ~serialize_to:name ~display_width:150 ~display_height:100 waveform
+  else ();
+  if not (List.equal Int.( = ) !all_outputs all_inputs)
+  then raise_s [%message "outputs did not match inputs"]
+;;
+
+let%expect_test "test" =
+  test
+    ~name:"/tmp/one_stop_bit_no_parity"
+    ~clock_frequency:200
+    ~baud_rate:200
+    ~include_parity_bit:false
+    ~stop_bits:1
+    ~all_inputs:[ 0b1010; 0b111; 0b1001_1001; 0b1111_1111; 0b0000_0000; 0b1010_1010 ];
+  [%expect {|
+     (10 7 153 255 0 170) |}];
+  test
+    ~name:"/tmp/one_stop_bit_with_parity"
+    ~clock_frequency:200
+    ~baud_rate:200
+    ~include_parity_bit:true
+    ~stop_bits:1
+    ~all_inputs:[ 0b1010; 0b111; 0b1001_1001; 0b1111_1111; 0b0000_0000; 0b1010_1010 ];
+  [%expect {| (10 7 153 255 0 170) |}];
+  test
+    ~name:"/tmp/two_stop_bits_with_parity"
+    ~clock_frequency:200
+    ~baud_rate:200
+    ~include_parity_bit:true
+    ~stop_bits:2
+    ~all_inputs:[ 0b1010; 0b111; 0b1001_1001; 0b1111_1111; 0b0000_0000; 0b1010_1010 ];
+  [%expect {| (10 7 153 255 0 170) |}]
+;;
