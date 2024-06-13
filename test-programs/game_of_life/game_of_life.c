47898a093ca251eace4e3677c06539284024d84e
Blake Loring <blake@parsed.uk>
2024-06-13 15:39:14 +0800

    Finish an implementation of conways game of life for the CPU

--- test-programs/game_of_life/game_of_life.c
+++ test-programs/game_of_life/game_of_life.c
@@ -1,4 +1,4 @@
-// Width must be a multiple of 8
+#define NULL 0
 #define WIDTH 80
 #define HEIGHT 24
 
@@ -14,39 +14,84 @@ void send_dma_l(char* msg, int len) {
   system_call(0, msg, len);
 }
 
+char ROW_BUFFER[BUFFER_SIZE];
 char BUFFER1[BUFFER_SIZE];
 char BUFFER2[BUFFER_SIZE];
 
-void memset(char* buffer, int size, char value) {
+void cmemset(char* buffer, int size, char value) {
   for (unsigned int i = 0; i < size; i++) {
     buffer[i] = value;
   }
 }
 
-char get(char* buffer, unsigned int x, unsigned int y) {
-  if (x > WIDTH) { return ' '; }
-  if (y > HEIGHT) { return ' '; }
+char* byte_address(char* buffer, unsigned int x, unsigned int y) {
+  if (x > WIDTH) { return NULL; }
+  if (y > HEIGHT) { return NULL; }
   int byte_index_x = x / 8;
-  int bit_x = 1 << (x % 8);
-  char* row = buffer + (y * (WIDTH / 8));
-  return (row[byte_index_x] & bit_x) != 0;
+  char* row = buffer + (y * (WIDTH / 8)) + byte_index_x;
+  return row;
+}
+
+unsigned int which_bit (unsigned int x) {
+  return 1 << (x % 8);
+}
+
+char get(char* buffer, unsigned int x, unsigned int y) {
+  char* addr = byte_address(buffer, x, y);
+
+  if (addr != NULL) {
+    return *addr & which_bit(x);
+  }
+  
+  return ' ';
+}
+
+void set(char* buffer, unsigned int x, unsigned int y, int value) {
+  char* addr = byte_address(buffer, x, y);
+  if (addr != NULL) {
+    if (value) {
+      *addr = *addr | which_bit(x);
+    } else {
+      *addr = *addr & !which_bit(x);
+    }
+  }
+}
+
+int neighbors(char* buffer, unsigned int x, unsigned int y) {
+  // This is a little yucky, but if x or y overflow they will
+  // fall out of bounds which will return an empty position upon
+  // get.
+  int sum = 0;
+  for (int yi = y - 1 ; yi <= y + 1; yi++) {
+    for (int xi = x - y; xi <= x + 1; yi++) {
+      if (xi != x && yi != y) {
+        sum += get(buffer, xi, yi);
+      }
+    }
+  }
 }
 
 void compute(char* next, char* prev) {
-  for (unsigned int i = 0; i < HEIGHT; i++) {
-    for (unsigned int i = 0; i < WIDTH; i++) {
-
+  // Compute the next state of the grid
+  for (unsigned int y = 0; y < HEIGHT; y++) {
+    for (unsigned int x = 0; x < WIDTH; x++) {
+      int c_neighbors = neighbors(prev, x, y);
+      set(next, x, y, c_neighbors > 1 && c_neighbors < 4); 
     }
   }
 }
 
 
-void expand_row(char* dst, char* src) {
+void expand_row(char* dst, char* buffer, int y) {
+  for (int i = 0; i < WIDTH; i++) {
+   dst[i] = get(buffer, i, y);
+  }
 }
 
 void send_rows(char* buffer) {
   for (int i = 0; i < HEIGHT; i++) {
-    send_dma_l(buffer + (i * WIDTH), WIDTH);
+    expand_row(ROW_BUFFER, buffer, i);
+    send_dma_l(ROW_BUFFER, WIDTH);
   }
 }
 
@@ -55,7 +100,7 @@ void c_start() {
   char* next = BUFFER2;
 
   for (;;) {
-    memset(next, BUFFER_SIZE, 0);
+    cmemset(next, BUFFER_SIZE, 0);
     compute(next, current);
     send_rows(next);
     char* tmp = current;
