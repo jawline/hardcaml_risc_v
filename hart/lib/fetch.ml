dae1b9010764a39950be053660b8ff979da38405
Blake Loring <blake@parsed.uk>
2026-03-09 23:38:40 +0800

    Fetch tweaks for sim

--- hart/lib/fetch.ml
+++ hart/lib/fetch.ml
@@ -69,11 +69,15 @@ struct
         ~f:(fun t -> mux2 valid ~:output_valid (t &: ~:output_valid))
         clock
     in
-    let registers =
-      Registers.For_writeback.Of_signal.reg ~enable:valid reg_spec_with_clear registers
-    in
     let maybe_reg =
       if Hart_config.register_fetch_output then reg reg_spec_with_clear else Fn.id
+    in
+    let registers =
+      (if Hart_config.register_fetch_output
+       then Registers.For_writeback.Of_signal.reg ~enable:valid
+       else Registers.For_writeback.Of_signal.cut_through_reg ~enable:valid)
+        reg_spec_with_clear
+        registers
     in
     { O.read_bus = prefetcher.read_bus
     ; valid = maybe_reg (output_valid &: (valid |: was_valid))
